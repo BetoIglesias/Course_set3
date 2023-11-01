d142a9019fad9da7bbf9dc8634b1b53f0649e0d2
BetoIglesias <betoiglesias010@gmail.com>
2023-11-01 16:40:46 -0300

    Solução simples para o exercicio proposto sobre Conjuntos - Set

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+HashSet<int> ints = new HashSet<int>();
+
+for (int letra = 0; letra < 3; letra++) {
+
+	Console.Write($"Quais os Estudantes do Curso {letra+1} ? ");
+	int na = int.Parse(Console.ReadLine());
+	for (int i = 0; i < na; i++) {
+		int matr = int.Parse(Console.ReadLine());
+		ints.Add(matr);
+	}
+}
+Console.WriteLine();
+Console.Write("Total de Estudantes : " + ints.Count);
